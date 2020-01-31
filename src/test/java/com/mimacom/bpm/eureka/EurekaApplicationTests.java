8cc24003f41d056ce88642f403d4bd835d770016
Javier Alfaya <francisco.alfaya@es.unisys.com>
2020-01-31 18:57:31 +0100

    first commit

---
+++ src/test/java/com/mimacom/bpm/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mimacom.bpm.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
